0aef899f05ded92dedbefe7986865e98c5f8b46d
Yoan-Alexander Grigorov <joan@acewebdesign.com.au>
2015-05-01 10:14:37 +0300

    Updating the readme file

--- database/dump.sql
+++ database/dump.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `shopping_cart` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `shopping_cart`;
--- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.24, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: shopping_cart
+-- Host: 127.0.0.1    Database: shopping_cart
 -- ------------------------------------------------------
--- Server version	5.5.38-0ubuntu0.14.04.1
+-- Server version	5.6.24-0ubuntu2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,15 +26,15 @@ DROP TABLE IF EXISTS `cart`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `cart` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sessionID` char(26) DEFAULT NULL,
   `product` int(11) DEFAULT NULL,
-  `sessionID` varchar(26) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `dateAdded` datetime DEFAULT NULL,
-  `quantityRequested` int(11) DEFAULT NULL,
+  `quantityRequested` int(10) unsigned DEFAULT NULL,
+  `dateAdded` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `fk_cart_1_idx` (`product`),
   KEY `session_id` (`sessionID`),
-  CONSTRAINT `FK_BA388B7D34A04AD` FOREIGN KEY (`product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  CONSTRAINT `fk_cart_1` FOREIGN KEY (`product`) REFERENCES `products` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -55,11 +55,11 @@ DROP TABLE IF EXISTS `discount_coupon`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `discount_coupon` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `code` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `discountRate` int(11) NOT NULL,
+  `code` varchar(45) DEFAULT NULL,
+  `discountRate` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `code` (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +68,7 @@ CREATE TABLE `discount_coupon` (
 
 LOCK TABLES `discount_coupon` WRITE;
 /*!40000 ALTER TABLE `discount_coupon` DISABLE KEYS */;
-INSERT INTO `discount_coupon` VALUES (1,'DISC22',25);
+REPLACE INTO `discount_coupon` VALUES (1,'DISC23',25),(2,'DISC28',40);
 /*!40000 ALTER TABLE `discount_coupon` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -83,14 +83,14 @@ CREATE TABLE `order_items` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `parentOrder` int(11) DEFAULT NULL,
   `product` int(11) DEFAULT NULL,
-  `quantityRequested` int(11) DEFAULT NULL,
-  `pricePurchased` double DEFAULT NULL,
+  `quantityRequested` int(10) unsigned DEFAULT NULL,
+  `pricePurchased` float(12,2) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_order_items_1_idx` (`parentOrder`),
   KEY `fk_order_items_2_idx` (`product`),
-  CONSTRAINT `FK_62809DB0F5299398` FOREIGN KEY (`parentOrder`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE SET NULL,
-  CONSTRAINT `FK_62809DB0D34A04AD` FOREIGN KEY (`product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE SET NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  CONSTRAINT `fk_order_items_1` FOREIGN KEY (`parentOrder`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE SET NULL,
+  CONSTRAINT `fk_order_items_2` FOREIGN KEY (`product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,7 +99,7 @@ CREATE TABLE `order_items` (
 
 LOCK TABLES `order_items` WRITE;
 /*!40000 ALTER TABLE `order_items` DISABLE KEYS */;
-INSERT INTO `order_items` VALUES (7,34,1,7,60),(8,34,2,3,150),(9,35,1,7,60),(10,35,2,3,150),(11,36,1,2,60),(12,36,2,4,150),(13,37,2,1,150);
+REPLACE INTO `order_items` VALUES (8,19,6,1,304.00),(9,20,8,1,201.00),(10,20,7,1,249.00);
 /*!40000 ALTER TABLE `order_items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -112,16 +112,16 @@ DROP TABLE IF EXISTS `orders`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `orderDate` datetime DEFAULT NULL,
-  `totalPrice` double DEFAULT NULL,
-  `firstName` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `lastName` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `address` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `city` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `country` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `phone` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `orderDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `totalPrice` float(12,2) DEFAULT NULL,
+  `firstName` varchar(45) DEFAULT NULL,
+  `lastName` varchar(45) DEFAULT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,7 +130,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (34,'2015-04-30 23:35:31',652.5,'Yoan','Grigorov','ddsadsadsadsa','Veliko Tarnovo','Bulgaria','0897871114'),(35,'2015-05-01 00:36:08',652.5,'Yoan','Grigorov','ddsadsadsadsa','Veliko Tarnovo','Bulgaria','0897871114'),(36,'2015-05-01 01:11:43',720,'Yoan','Grigorov','ddsadsadsadsa','Veliko Tarnovo','Bulgaria','0897871114'),(37,'2015-05-01 01:12:41',150,'Yoan','Grigorov','ddsadsadsadsa','Veliko Tarnovo','Bulgaria','0897871114');
+REPLACE INTO `orders` VALUES (18,'2015-05-01 04:02:35',0.00,NULL,NULL,NULL,NULL,NULL,NULL),(19,'2015-05-01 04:05:24',304.00,'Joan','Grigorov','7 Maxim Raykovich street','Drezden','Bulgaria','(123) 142-5122'),(20,'2015-05-01 04:07:03',337.50,'Joan','Grigorov','7 Maxim Raykovich street','Drezden','Bulgaria','(123) 142-5122');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -143,14 +143,14 @@ DROP TABLE IF EXISTS `products`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `shortDescription` varchar(225) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `fullDescription` text COLLATE utf8_unicode_ci,
-  `photo` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `price` double DEFAULT NULL,
-  `quantityAvailable` int(11) DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `shortDescription` varchar(225) DEFAULT NULL,
+  `fullDescription` text,
+  `price` float(12,2) DEFAULT NULL,
+  `photo` varchar(45) DEFAULT NULL,
+  `quantityAvailable` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -159,7 +159,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Test product','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a','\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n\r\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n\r\n \r\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32',NULL,60,65),(2,'Lorem ipsum','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a','\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\r\n\r\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\r\n\r\n \r\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32',NULL,150,100);
+REPLACE INTO `products` VALUES (6,'Odyssey OAS130 Alto Saxophone Outfit','The OdysseyTM Alto SaXophone SaXophone is a tremendously popular entry-level choice featuring a durable, attractive polished gold clear lacquer brass body, engraved bell.','Product Description\r\n\r\nThe OdysseyTM Alto SaXophone SaXophone is a tremendously popular entry-level choice featuring a durable, attractive polished gold clear lacquer brass body, engraved bell, power forged keywork, stainless steel rods, springs and pins, pearloid keycaps, high F# key, single braced pad cup on low C, fine tuning adjustment, quality pads and mouthpiece, adjustable thumb rest. Supplied with cork grease, locking deluxe neck strap, reed and instruction guide. The ABS Hard Case is lightweight, strong and durable with a plush lined, snug-fit black interior and comfort carry handle. Absolutely ideal for novices & beginners who are just starting out, the Odyssey OAS130 is an affordable, popular choice throughout the UK.\r\n\r\nManufacturer\'s Description\r\nGenerations of experience, precise design, modern hi-tech manufacturing techniques, careful quality control, consistent attention to detail, and a personal touch are all hallmarks of Oddyssey instruments.',304.00,NULL,20),(7,'\'Paolo Mark\' Alto','Stunning \'Paolo Mark\' Alto Saxophone with Case + Accessories Extremely nice tone given the low price Precision engineering, easy action and accurate intonation','Stunning \'Paolo Mark\' Alto Saxophone with Case + Accessories Extremely nice tone given the low price Precision engineering, easy action and accurate intonation Perfect for beginner / intermediate and student musicians. Exceptional value! Finish: Gold Lacquer Instrument Level: Beginner / Student / Intermediate Case: Soft Case with Luxury Soft Interior Accessories: Mouthpiece & Reed, Cloth, Cleaning Rod, White Gloves, Neck Strap',249.00,NULL,20),(8,'Windsor Alto Saxophone','Windsor have done it again and designed an entry level alto saxophone at an affordable price for beginners, ideal for any budding player who is looking to take up this magnificant instrument.','Product Description\r\nWindsor have done it again and designed an entry level alto saxophone at an affordable price for beginners, ideal for any budding player who is looking to take up this magnificant instrument. Complete with everything you need to get playing straight out of the box, the impressive Windsor alto sax comes in a fully lined plush hard case for maximum protection of your instrument whilst on the move, as well as including a strap and cleaning kit. The Windsor alto saxophone is ideal for novice players, offering a great instrument, well designed and manufactured to ensure comfortable playability and a superb tone. Features: Gold laquer finish Cleaning kit Strap Mouthpiece Ligature Reed Plush lined hard case\r\nManufacturer\'s Description\r\nThe Windsor Alto Saxophone is the perfect instrument for students, enthusiasts and even players looking to purchase a second instrument at a great price. This instrument is manufactured under strict quality controls and easily compares to other well-known brands but at a fraction of the cost.\r\n\r\nA cleaning kit, mouthpiece with reed and cover is included as well as a plush lined durable Plush Lined Case. It has a one year manufacturers warranty against any manufacturing defects so you can buy with complete confidence .',201.00,NULL,25);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -172,4 +172,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-05-01  1:14:01
+-- Dump completed on 2015-05-01 10:07:46
